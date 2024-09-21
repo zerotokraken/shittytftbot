00eda1333f914a98fb88044c58fa8f67551fca53
zerotokraken <ztk.cmg@gmail.com>
2024-09-21 15:02:40 -0400

    Add appspec.yml

--- run_main.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-# Execute the main.py script
-subprocess.run(["python", "main.py"])
---
+++ run_main.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the directory where the app is installed
+cd /home/ec2-user/ShittyTFTBot  # Adjust if needed
+
+# Run your main.py script
+python3 main.py
