b160d44c9a09bcad1ab8c18058327df13834295d
zerotokraken <ztk.cmg@gmail.com>
2024-09-24 12:54:38 -0400

    modified load_cogs

--- bot/cogs/roll.py
+++ bot/cogs/roll.py
@@ -98,5 +98,5 @@ class RollCommands(commands.Cog):
             await ctx.send("No images available to display.")
 
 async def setup(bot, champions_data, latest_version, shop_odds):
-    cog = RollCommands(bot, champions_data, latest_version, shop_odds)
-    bot.add_cog(cog)
+    await bot.add_cog(MaldingCommand(bot, champions_data, latest_version, shop_odds))
+
