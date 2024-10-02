8bd0f3006e13b31f4ef905ca9f286baa85a226cb
zerotokraken <ztk.cmg@gmail.com>
2024-10-01 23:38:41 -0400

    Update misc.py

--- bot/cogs/misc.py
+++ bot/cogs/misc.py
@@ -143,7 +143,8 @@ class MiscCommands(commands.Cog):
     async def saida(self, ctx):
         lines_saida = [
             "https://clips.twitch.tv/ConcernedPoliteFlyShadyLulu-ZYsvLju7ZH8kualJ?embedo",
-            "https://clips.twitch.tv/EnjoyableGlamorousWalletHeyGuys-04szKrShCTmHm74L?embedo"
+            "https://clips.twitch.tv/EnjoyableGlamorousWalletHeyGuys-04szKrShCTmHm74L?embedo",
+            "https://clips.twitch.tv/embed?clip=BrainyRacyPoxOSsloth-3hAamOJIS7cvqKyn?embed0"
         ]
         selected_line_saida = random.choice(lines_saida)
         await ctx.send(f'{selected_line_saida}')
