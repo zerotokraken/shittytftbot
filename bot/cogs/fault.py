bdf87e23d606cd402b0f75e5f9b7eace744797b1
zerotokraken <ztk.cmg@gmail.com>
2024-10-08 03:55:16 -0400

    Fault cog changed to distinct messages

--- bot/cogs/fault.py
+++ bot/cogs/fault.py
@@ -24,14 +24,14 @@ class FaultCommand(commands.Cog):
         try:
             cursor = self.conn.cursor()
             query = """
-                SELECT DISTINCT content FROM (
-                    SELECT content FROM general_messages WHERE content ILIKE '%is it even my fault%' AND author_id != 1285268322551726140
+                SELECT content FROM (
+                    SELECT DISTINCT content, RANDOM() AS rand FROM general_messages WHERE content ILIKE '%is it even my fault%' AND author_id != 1285268322551726140
                     UNION ALL
-                    SELECT content FROM advice_messages WHERE content ILIKE '%is it even my fault%' AND author_id != 1285268322551726140
+                    SELECT DISTINCT content, RANDOM() AS rand FROM advice_messages WHERE content ILIKE '%is it even my fault%' AND author_id != 1285268322551726140
                     UNION ALL
-                    SELECT content FROM malding_messages WHERE content ILIKE '%is it even my fault%' AND author_id != 1285268322551726140
+                    SELECT DISTINCT content, RANDOM() AS rand FROM malding_messages WHERE content ILIKE '%is it even my fault%' AND author_id != 1285268322551726140
                 ) AS combined
-                ORDER BY RANDOM()
+                ORDER BY rand
                 LIMIT 1;
             """
             cursor.execute(query)
