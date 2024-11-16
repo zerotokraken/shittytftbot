c45a7cb689132af0b18a3c5edc730405ef748141
zerotokraken <ztk.cmg@gmail.com>
2024-11-16 15:16:34 -0500

    modified timeout cog

--- bot/cogs/timeout.py
+++ bot/cogs/timeout.py
@@ -39,10 +39,11 @@ class Timeout(commands.Cog):
 
         # If no member found, attempt case-insensitive search
         if not member:
-            member = discord.utils.get(ctx.guild.members, nick=lambda m: m.nick.lower() == member_name.lower())
-            if not member:
-                print("Member not found. Please check the username and try again.")
-                return
+            member = discord.utils.get(ctx.guild.members,
+                                       lambda m: m.nick and m.nick.lower() == member_name.lower())
+        if not member:
+            print("Member not found. Please check the username and try again.")
+            return
 
         total_seconds = self.parse_duration(duration)
         if total_seconds is None:
